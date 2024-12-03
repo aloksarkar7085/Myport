b45fa5e6cf103f37f3f84326c9c0097dc6d34791
ALOK SARKAR <97972813+aloksarkar7085@users.noreply.github.com>
2024-12-03 13:36:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+   /* let typed = new Typed('.multiple-text',{
+      strings:['Web Developer.','Software Engineer.','Editor.','Code Lover.','DSA Lover.'],
+typeSpeed:70,
+backSpeed:70,
+backDelay:1000,
+loop:true,
+    });*/
+// Typed.js Animation
+let typed = new Typed('.multiple-text', {
+    strings: ['Web Developer.', 'Software Engineer.', 'Editor.', 'Code Lover.', 'DSA Lover.'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
