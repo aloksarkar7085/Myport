a740ae300d4a3a66036dd014c4894bb8ba56d241
ALOK SARKAR <97972813+aloksarkar7085@users.noreply.github.com>
2025-04-03 16:32:46 +0530

    Update script.js
    add ABAP DEV

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ backDelay:1000,
 loop:true,
     });*/
 // Typed.js Animation
+/*let typed = new Typed('.multiple-text', {
+    strings: ['SAP ABAP DEV.', 'Web Developer.', 'Software Engineer.', 'Editor.', 'Code Lover.', 'DSA Lover.'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});*/
 let typed = new Typed('.multiple-text', {
     strings: ['SAP ABAP DEV.', 'Web Developer.', 'Software Engineer.', 'Editor.', 'Code Lover.', 'DSA Lover.'],
     typeSpeed: 70,
