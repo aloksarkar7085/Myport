b906309627b4343488c18edff3b140657f2b60a2
ALOK SARKAR <97972813+aloksarkar7085@users.noreply.github.com>
2025-04-03 16:28:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ loop:true,
     });*/
 // Typed.js Animation
 let typed = new Typed('.multiple-text', {
-    strings: ['Web Developer.', 'Software Engineer.', 'Editor.', 'Code Lover.', 'DSA Lover.'],
+    strings: ['SAP ABAP DEV.', 'Web Developer.', 'Software Engineer.', 'Editor.', 'Code Lover.', 'DSA Lover.'],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay: 1000,
